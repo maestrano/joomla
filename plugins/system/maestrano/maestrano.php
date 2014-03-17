901738b100ea25fa93498cdfe9ec6a0cbfd7bc4a
Arnaud Lachaume <arnaud.lachaume@welwire.com>
2014-03-17 21:04:59 +1100

    fixed logout and session management

--- plugins/system/maestrano/maestrano.php
+++ plugins/system/maestrano/maestrano.php
@@ -2,7 +2,7 @@
 /**
  * @version    3.1.0
  * @package    Joomla.Plugin
- * @subpackage Authentication.joomla
+ * @subpackage System.SSO
  * @license    GNU/GPL
  */
 
@@ -23,117 +23,35 @@ $root = realpath(dirname(__FILE__).DS.'..'.DS.'..'.DS.'..');
 require_once $root . '/maestrano/app/init/base.php';
 $maestrano = MaestranoService::getInstance();
 
-$uri = clone JUri::getInstance();
-var_dump($uri->getQuery());
-$params = parse_str($uri->getQuery());
-var_dump($params);
-echo "<br/><br/>";
-var_dump($app->getRouter());
-exit;
 
+// Check Maestrano session and perform
+// redirects based on context
 if ($maestrano->isSsoEnabled()) {
-  // Get User
-  $user = JFactory::getUser();
   
-  // Check mno session is still valid
-  // (User redirected to SSO automatically if not
-  // logged in)
-  if (!$maestrano->getSsoSession()->isValid()) {
-    header("Location: " . $maestrano->getSsoInitUrl());
+  // Destroy Session and
+  // Redirect to Maestrano logout page 
+  // if action is logout
+  $params = JUri::getInstance()->getQuery(true);
+  if ($params && $params['task'] == 'logout') {
+    $session = JFactory::getSession();
+    $session->destroy();
+    session_unset();
+    session_destroy();
+    
+    header("Location: " . $maestrano->getSsoLogoutUrl());
     exit;
   }
   
-  // if ($user->id) {
-  //   if (!$maestrano->getSsoSession()->isValid()) {
-  //     header("Location: " . $maestrano->getSsoInitUrl());
-  //     exit;
-  //   }
-  //   error_log("MNO SESSION IS VALID!");
-  // } else {
-  //   error_log("USER NOT LOGGED IN!");
-  // }
+  // Get User
+  $user = JFactory::getUser();
+  
+  // Check user is logged in and mno session is still valid
+  // (User redirected to SSO automatically if not
+  // logged in)
+  if (!$user->id || !$maestrano->getSsoSession()->isValid()) {
+    header("Location: " . $maestrano->getSsoInitUrl());
+    exit;
+  }
 }
-
-// class plgAuthenticationMaestrano extends JPlugin
-// {
-//     
-//     public function onUserLogout() 
-//     {
-//       error_log("WE ARE IN THE MNO onUserLogout METHOD!!!");
-//     }
-//     
-//     // 
-//     // public function onUserAuthorisation() 
-//     // {
-//     //   JLog::add(JText::_("WE ARE IN THE MNO onUserAuthorisation METHOD!!!"), JLog::INFO, 'jerror');
-//     //   error_log("WE ARE IN THE MNO onUserAuthorisation METHOD!!!");
-//     // }
-//     
-//     /**
-//      * This method handles authentication via Maestrano
-//      *
-//      * @access    public
-//      * @param     array     $credentials    Array holding the user credentials ('username' and 'password')
-//      * @param     array     $options        Array of extra options
-//      * @param     object    $response       Authentication response object
-//      * @return    boolean
-//      * @since 1.5
-//      */
-//     // public function onUserAuthenticate( $credentials, $options, &$response )
-//     // {
-//     //     error_log("WE ARE IN THE MNO onUserAuthenticate METHOD!!!");
-//     //     /*
-//     //      * Here you would do whatever you need for an authentication routine with the credentials
-//     //      *
-//     //      * In this example the mixed variable $return would be set to false
-//     //      * if the authentication routine fails or an integer userid of the authenticated
-//     //      * user if the routine passes
-//     //      */
-//     //     $query  = $this->db->getQuery(true)
-//     //             ->select('id')
-//     //             ->from('#__users')
-//     //             ->where('username=' . $db->quote($credentials['username']));
-//     //  
-//     //     $this->db->setQuery($query);
-//     //     $result = $this->db->loadResult();
-//     //  
-//     //     if (!$result) {
-//     //         $response->status = STATUS_FAILURE;
-//     //         $response->error_message = 'User does not exist';
-//     //     }
-//     //  
-//     //     /**
-//     //      * To authenticate, the username must exist in the database, and the password should be equal
-//     //      * to the reverse of the username (so user joeblow would have password wolbeoj)
-//     //      */
-//     //     if($result && ($credentials['username'] == $credentials['password']))
-//     //     {
-//     //         $email = JUser::getInstance($result); // Bring this in line with the rest of the system
-//     //         $response->email = $email->email;
-//     //         $response->status = JAuthentication::STATUS_SUCCESS;
-//     //     }
-//     //     else
-//     //     {
-//     //         $response->status = JAuthentication::STATUS_FAILURE;
-//     //         $response->error_message = 'Invalid username and password';
-//     //     }
-//     // }
-// }
-
-
-
-
-
-// Login page should redirect to SSO automatically
-
-
-
-/**
- * Maestrano Authentication Plugin
- *
- * @package    Joomla.Plugin
- * @subpackage Authentication.joomla
- * @license    GNU/GPL
- */
 
 ?>
